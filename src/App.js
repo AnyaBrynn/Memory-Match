da353e11104e84a37dfc69888a9a81709ceef7b9
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-07-24 16:30:17 -0700

    MDisplay cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
+import { useState } from 'react'
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/bee.png" },
+  { "src": "/img/buffalo.png" },
+  { "src": "/img/bullfinch.png" },
+  { "src": "/img/chameleon.png" },
+  { "src": "/img/clown-fish.png" },
+  { "src": "/img/crab.png" },
+  { "src": "/img/fox.png" },
+  { "src": "/img/frog.png" },
+  { "src": "/img/shark.png" },
+
+]
+
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // shuffle cards 
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+      
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
   return (
     <div className="App">
       <h1>Animal Memory Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-layout">
+        {cards.map(card => (
+            <div className="card" key={card.id}>
+              <div>
+                <img className="front" src={card.src} alt="card front" />
+                <img className="back" src="/img/stars.png" alt="card back" />
+              </div>
+            </div>
+          ))}
+      </div>
+
     </div>
   );
 }
